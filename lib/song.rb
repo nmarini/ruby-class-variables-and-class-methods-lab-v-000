f7dbcfb670f1057acd41c760bdb5f62034ebf4d6
Nicolas Marini <nmarini44@gmail.com>
2018-06-12 23:02:19 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :name, :artist, :genre 
+
+  @@count = 0
+  @@genres = 0
+  @@artists = 0
+  
+    def initialize(name, artist, genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+    end 
+
+    
+
+
+
+end
