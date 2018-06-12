a00fd9abbc769a62045ce06326ae2190584db3b6
Nicolas Marini <nmarini44@gmail.com>
2018-06-12 23:29:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,31 @@ class Song
 
       @@count += 1
 
+      @@genres << @genre
+      @@artists << @artist
+ 
+
+
     end
 
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    a = [];
+    @@artists.each{|artist| a << artist unless a.include?(artist)};
+    a;   
+    
+  end
+
+  def self.genres
+   g = [];
+    @@genres.each{|genre| g << genre unless g.include?(genre)};
+    g;   
+    
+  end
 
 
 
